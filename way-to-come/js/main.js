d98198bdea00394dd2d9d82f2195d46cd7562b59
Hong Gayoung <thehd9891@naver.com>
2022-03-15 13:12:13 +0900

    add

--- way-to-come/js/main.js
+++ way-to-come/js/main.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', (e) => {
-
+    myMap()
 });
+let map;
+
+function myMap(){
+    var myLatLng = { lat: 35.125824, lng: 126.910878 }
+    map = new google.maps.Map(document.getElementById("googleMap"), {
+        center: myLatLng,
+        zoom: 17,
+    });
+    new google.maps.Marker({
+        position: myLatLng, map
+    });
+ }

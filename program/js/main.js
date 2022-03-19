7633982b6fcac853aec6eefa8b17bd7d2856c25f
Hong Gayoung <51188309+dudrbrb@users.noreply.github.com>
2022-03-19 15:15:07 +0900

    Update main.js

--- program/js/main.js
+++ program/js/main.js
@@ -1,7 +1,26 @@
 window.addEventListener('load', (e) => {
+    clickProgram();
     if(window.location.href.split('?')[1] == 'pl'){
         var plates = document.querySelector('header.pl');
 
         plates.scrollIntoView({behavior: "smooth", block: "start"});
     }
 });
+
+
+function clickProgram() {
+    var btns = document.querySelectorAll('.program-list li');
+
+    [].forEach.call(btns, function(e){ 
+        e.addEventListener("click", function(){
+            var program = e.getAttribute('data-name');
+            var section = '';
+
+            program == 'pt'
+                ? section = document.querySelector('header.pt')
+                : section = document.querySelector('header.pl');
+                
+            section.scrollIntoView({behavior: "smooth", block: "start"});
+        }, false); 
+    });
+}
